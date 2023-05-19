aba96c2d88fc34a6897947741c80904a1aedaf58
ErykTavares <erictavares35@gmail.com>
2023-05-19 14:59:22 -0300

    feat: create readme

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import DefaultLayout from '@/layout/defaultLayout';
-import * as S from '../style/pages/home';
+import * as S from '@/style/pages/home';
 
 const Home = (): JSX.Element => (
 	<DefaultLayout>
