39cfffcf91da78475d869f6b610d01c22124b728
ErykTavares <erictavares35@gmail.com>
2023-05-23 20:49:03 -0300

    style: home  style some adjustments

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,48 @@
+import NewsCard from '@/components/newsCard';
 import DefaultLayout from '@/layout/defaultLayout';
 import * as S from '@/style/pages/home';
+import axios from 'axios';
+import dateFormat from 'dateformat';
+import { GetServerSideProps } from 'next';
+import { useRouter } from 'next/router';
 
-const Home = (): JSX.Element => (
-	<DefaultLayout>
-		<S.Container>
-			<h2>home here</h2>
-		</S.Container>
-	</DefaultLayout>
-);
+interface IHomeProps {
+	data: DNews.INews[];
+}
+
+const Home = ({ data }: IHomeProps): JSX.Element => {
+	const router = useRouter();
+	console.log(data);
+
+	return (
+		<DefaultLayout>
+			<S.Container>
+				<h2>Últimas Notícias</h2>
+				{/* <button
+					type='button'
+					onClick={() => {
+						router.push(`${router.pathname}?state=saoPaulo`);
+					}}
+				>
+					change filters
+				</button> */}
+				<S.Wrapper>
+					{data.map((item) => (
+						<NewsCard news={item} />
+					))}
+				</S.Wrapper>
+			</S.Container>
+		</DefaultLayout>
+	);
+};
+
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+	console.log(query);
+	const currentDate = dateFormat(new Date(), 'dd/mm/yyyy');
+	const data = await axios(
+		`https://apinoticias.tedk.com.br/api/?q=${query?.state || 'bahia'}&date=${currentDate}`
+	).then((response) => response.data.list);
+
+	return { props: { data } };
+};
 export default Home;
--- src/style/pages/home.ts
+++ src/style/pages/home.ts
@@ -1,8 +1,32 @@
 import styled from 'styled-components';
 
 export const Container = styled.section`
+	display: flex;
+	flex-direction: column;
+	align-items: start;
+	justify-content: start;
+	padding: 1.2rem 2rem;
+
+	@media (max-width: 768px) {
+		padding: 1.2rem 1rem;
+	}
+
 	h2 {
-		color: red;
-		font-weight: bolder;
+		width: 100%;
+		font-size: 2.25rem;
+		font-weight: bold;
+		align-self: start;
+	}
+`;
+
+export const Wrapper = styled.div`
+	width: 100%;
+	display: grid;
+	grid-template-columns: repeat(auto-fit, minMax(400px, auto));
+	place-items: start;
+	gap: 1rem;
+
+	@media screen and (max-width: 465px) {
+		grid-template-columns: 1fr;
 	}
 `;
