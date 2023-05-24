32f2d3e78c218d1636653b56a13cd3842d62f939
ErykTavares <erictavares35@gmail.com>
2023-05-24 18:32:35 -0300

    feat:  some adjustments

--- src/layout/defaultLayout.tsx
+++ src/layout/defaultLayout.tsx
@@ -2,15 +2,21 @@ import React from 'react';
 import { Roboto } from 'next/font/google';
 import Header from '@/components/header';
 import Footer from '@/components/footer';
+import Head from 'next/head';
 
 const roboto = Roboto({ weight: '400', subsets: ['latin'] });
 
 interface IDefaultProps {
 	children: JSX.Element;
+	title: string;
 }
 
-const DefaultLayout = ({ children }: IDefaultProps): JSX.Element => (
+const DefaultLayout = ({ children, title }: IDefaultProps): JSX.Element => (
 	<main className={roboto.className}>
+		<Head>
+			<title>{title}</title>
+			<meta property='og:title' content={title} key='title' />
+		</Head>
 		<Header />
 		{children}
 		<Footer />
